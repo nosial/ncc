16859642325e9ce8e5f2d5b1251deaaf2e377a66
Netkas <zixing.narrakas1996@gmail.com>
2022-07-09 20:04:28 -0400

    Updated bootstrap.php

--- src/ncc/bootstrap.php
+++ src/ncc/bootstrap.php
@@ -2,3 +2,6 @@
 
     // polyfill-mbstring
     require_once(__DIR__ . DIRECTORY_SEPARATOR . 'ThirdParty' . DIRECTORY_SEPARATOR . 'Symfony' . DIRECTORY_SEPARATOR . 'polyfill-mbstring' . DIRECTORY_SEPARATOR . 'bootstrap.php');
+
+    // polyfill-ctype
+    require_once(__DIR__ . DIRECTORY_SEPARATOR . 'ThirdParty' . DIRECTORY_SEPARATOR . 'Symfony' . DIRECTORY_SEPARATOR . 'polyfill-ctype' . DIRECTORY_SEPARATOR . 'bootstrap.php');
