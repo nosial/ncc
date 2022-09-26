af819913fd6456a6f0344dba76252e4d214d16a4
Netkas <zixing.narrakas1996@gmail.com>
2022-09-26 17:45:04 -0400

    Updated autoload.php for tests

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,8 +1,12 @@
 <?php
 
-    $SourceDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'ncc';
+    $BuildDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'build';
+    $AutoloadPath = $BuildDirectory . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-    if(file_exists($SourceDirectory . DIRECTORY_SEPARATOR . 'autoload.php') == false)
-        throw new RuntimeException('The autoload file was not found in \'' . $SourceDirectory . '\'');
+    if(!file_exists($BuildDirectory) || !is_dir($BuildDirectory))
+        throw new RuntimeException('Build directory does not exist, to run tests you must build the project.');
 
-    require($SourceDirectory . DIRECTORY_SEPARATOR . 'autoload.php');
+    if(!file($AutoloadPath) || is_file($AutoloadPath))
+        throw new RuntimeException('Autoload file does not exist in \'' . $BuildDirectory .'\', to run tests you must build the project.');
+
+    require($AutoloadPath);
