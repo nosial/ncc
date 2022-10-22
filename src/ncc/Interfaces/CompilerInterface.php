5016b42759217eba82e39cf459ae88e76fbd94d2
Netkas <zixing.narrakas1996@gmail.com>
2022-10-22 04:30:27 -0400

    Updated CompilerInterface.php

--- src/ncc/Interfaces/CompilerInterface.php
+++ src/ncc/Interfaces/CompilerInterface.php
@@ -2,9 +2,26 @@
 
     namespace ncc\Interfaces;
 
+    use ncc\Abstracts\Options\BuildConfigurationValues;
+
     interface CompilerInterface
     {
-        public function prepare(array $options, string $src);
+        /**
+         * Prepares the package for the build process, this method is called before build()
+         *
+         * @param array $options Options to pass on to the compiler
+         * @param string $path The path that the project file is located in (project.json)
+         * @param string $build_configuration The build configuration to use to build the project
+         * @return void
+         */
+        public function prepare(array $options, string $path, string $build_configuration=BuildConfigurationValues::DefaultConfiguration): void;
 
-        public function build(array $options, string $src);
+        /**
+         * Builds the package, returns the output path of the build
+         *
+         * @param array $options Options to pass on to the compiler
+         * @param string $path The path that the project file is located in (project.json)
+         * @return string Returns the output path of the build
+         */
+        public function build(array $options, string $path): string;
     }
