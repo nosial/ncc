b751b39548edc07bc91d04442dfbc4e3b76343d9
Netkas <netkas@nosial.net>
2023-08-30 20:04:51 -0400

    Moved old tests

--- tests/old_tests/third_party/theseer/directory_scanner.php
+++ tests/old_tests/third_party/theseer/directory_scanner.php
@@ -3,7 +3,7 @@
     require(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php');
 
     $Scanner = new \ncc\ThirdParty\theseer\DirectoryScanner\DirectoryScanner();
-    $Basedir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..');
+    $Basedir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'third_party');
 
     /** @var SplFileInfo $item */
     foreach($Scanner($Basedir . DIRECTORY_SEPARATOR . 'src', true) as $item)
