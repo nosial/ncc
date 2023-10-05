41ec00d136b21b0efcfeb3c90cee62e35b0cfa89
Cybersecbyte [UFOLINUX] <cybersecbyte@gmail.com>
2023-10-05 17:33:09 +0000

    Remove repo downloading and use local files

--- src/debian/install.sh
+++ src/debian/install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
-
-# Download redistributable source
-wget -O ncc-2.0.0.tar.gz https://git.n64.cc/nosial/ncc/-/archive/dev/ncc-dev.tar.gz
-
-# Extract and install
-tar -xzvf ncc-2.0.0.tar.gz
-mv ncc-dev ncc-2.0.0
-cd ncc-2.0.0
+# compile and make ta
+cd ..
 make redist tar
+
+# extract compiled tar to /usr/share/ncc directory 
 sudo mkdir -p /usr/share/ncc
 sudo tar -xzvf build/ncc_2.0.0.tar.gz -C /usr/share/ncc/
