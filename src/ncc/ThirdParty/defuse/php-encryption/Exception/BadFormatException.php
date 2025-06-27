9fdac955f09c14abd2dd9105c023999547c9e7c0
netkas <netkas.nosial@gmail.com>
2025-06-27 13:44:26 -0400

    chore: add Symfony polyfill and third-party submodules to project

--- src/ncc/ThirdParty/defuse/php-encryption/Exception/BadFormatException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ncc\Defuse\Crypto\Exception;
-
-class BadFormatException extends \ncc\Defuse\Crypto\Exception\CryptoException
-{
-}
--- src/ncc/ThirdParty/defuse/php-encryption/Exception/WrongKeyOrModifiedCiphertextException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ncc\Defuse\Crypto\Exception;
-
-class WrongKeyOrModifiedCiphertextException extends \ncc\Defuse\Crypto\Exception\CryptoException
-{
-}
