f57b912dbd527776f9df99f03bbe23f82ab3402e
netkas <netkas.nosial@gmail.com>
2025-11-25 15:05:50 -0500

    Added AbstractProjectConverter for building project converters

---
+++ src/ncc/Abstracts/AbstractProjectConverter.php
@@ -0,0 +1,38 @@
+<?php
+    /*
+     * Copyright (c) Nosial 2022-2025, all rights reserved.
+     *
+     *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+     *  associated documentation files (the "Software"), to deal in the Software without restriction, including without
+     *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+     *  Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+     *  conditions:
+     *
+     *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+     *  of the Software.
+     *
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+     *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+     *  PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+     *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+     *  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+     *  DEALINGS IN THE SOFTWARE.
+     *
+     */
+
+    namespace ncc\Abstracts;
+
+    use Exception;
+    use ncc\Objects\Project;
+
+    abstract class AbstractProjectConverter
+    {
+        /**
+         * Converts the given project configuration file into a ncc v3+ configuration file
+         *
+         * @param string $filePath The filepath of the configuration file to convert
+         * @return Project The resulting generated Project configuration based off the configuration file
+         * @throws Exception thrown if there was an error converting the project configuration
+         */
+        public abstract function convert(string $filePath): Project;
+    }
