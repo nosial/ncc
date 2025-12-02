fe99198c5df43343f0f694bc267822c4d0f4f6ef
netkas <netkas.nosial@gmail.com>
2025-12-01 22:17:27 -0500

    Add ProjectType enum to define project file types and converters

---
+++ src/ncc/Enums/ProjectType.php
@@ -0,0 +1,97 @@
+<?php
+    /*
+     * Copyright (c) Nosial 2022-2025, all rights reserved.
+     *
+     *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+     *  associated documentation files (the "Software"), to deal in the Software without restriction, including without
+     *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+     *  Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+     *  conditions:
+     *
+     *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+     *  of the Software.
+     *
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+     *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+     *  PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+     *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+     *  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+     *  DEALINGS IN THE SOFTWARE.
+     *
+     */
+
+    namespace ncc\Enums;
+
+    use FilesystemIterator;
+    use ncc\Abstracts\AbstractProjectConverter;
+    use ncc\ProjectConverters\ComposerProjectConverter;
+    use ncc\ProjectConverters\LegacyProjectConverter;
+    use RecursiveDirectoryIterator;
+    use RecursiveIteratorIterator;
+
+    enum ProjectType : string
+    {
+        case NCC = 'project.yml';
+        case NCC_V2 = 'project.json';
+        case COMPOSER = 'composer.json';
+        case NONE = 'none';
+
+        /**
+         * Gets the full file path of the project file if it exists in the given path.
+         *
+         * @param string $path The path to search for the project file.
+         * @return string|null The full file path if found, null otherwise.
+         */
+        public function getFilePath(string $path): ?string
+        {
+            foreach ((new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::SELF_FIRST)) as $file)
+            {
+                if ($file->isFile() && $file->getFilename() === $this->value)
+                {
+                    return $file->getPathname();
+                }
+            }
+
+            return null;
+        }
+
+        /**
+         * Gets the appropriate project converter for the project type.
+         *
+         * @return AbstractProjectConverter|null The project converter instance or null if not applicable.
+         */
+        public function getConverter(): ?AbstractProjectConverter
+        {
+            return match ($this)
+            {
+                self::NCC_V2 => new LegacyProjectConverter(),
+                self::COMPOSER => new ComposerProjectConverter(),
+                default => null
+            };
+        }
+
+        /**
+         * Detects the project type based on the presence of specific files in the given path.
+         *
+         * @param string $path The path to check for project files.
+         * @return ProjectType The detected project type.
+         */
+        public static function detectProjectType(string $path): ProjectType
+        {
+            foreach (self::cases() as $case)
+            {
+                if ($case === self::NONE)
+                {
+                    continue;
+                }
+
+                $filePath = $case->getFilePath($path);
+                if ($filePath !== null)
+                {
+                    return $case;
+                }
+            }
+
+            return self::NONE;
+        }
+    }
