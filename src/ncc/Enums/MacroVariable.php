30a7d1516609a27d87654ba7c26e665c3bf1e68b
netkas <netkas.nosial@gmail.com>
2025-12-01 22:16:23 -0500

    Enhance MacroVariable enum with additional datetime and runtime macros

--- src/ncc/Enums/MacroVariable.php
+++ src/ncc/Enums/MacroVariable.php
@@ -26,22 +26,59 @@
 
     enum MacroVariable : string
     {
-        // Generic Macros
-        case DATE = '${DATE}';
-        case TIME = '${TIME}';
-        case DATETIME = '${DATETIME}';
-        case YEAR = '${YEAR}';
-        case MONTH = '${MONTH}';
-        case DAY = '${DAY}';
-        case HOUR = '${HOUR}';
-        case MINUTE = '${MINUTE}';
-        case SECOND = '${SECOND}';
+        // Datetime Macros
+        case d = '${d}'; // Day of the month, 2 digits with leading zeros  01 through 31
+        case D = '${D}'; // A textual representation of a day, three letters Mon through Sun
+        case j = '${j}'; // Day of the month without leading zeros 1 through 31
+        case l = '${l}'; // A full textual representation of the day of the week Sunday through Saturday
+        case N = '${N}'; // ISO 8601 numeric representation of the day of the week 1 (Monday) to 7 (Sunday)
+        case S = '${S}'; // English ordinal suffix for the day of the month, 2 characters st, nd, rd, th
+        case w = '${w}'; // Numeric representation of the day of the week 0 (sunday) through 6 (Saturday)
+        case z = '${z}'; // The day of the year (starting from 0) 0 through 365
+        // Week Format
+        case W = '${W}'; // ISO 8601 week number of year, weeks starting on Monday 42 (42nd week in year)
+        // Month Format
+        case F = '${F}'; // A full textual representation of a month, such as January or March January through December
+        case m = '${m}'; // Numeric representation of a month, with leading zeros 01 through 12
+        case M = '${M}'; // A short textual representation of a month, three letters Jan through Dec
+        case n = '${n}'; // Numeric representation of a month, without leading zeros 1 through 12
+        case t = '${t}'; // Number of days in the given month 28 through 31
+        // Year format
+        case L = '${L}'; // Whether it's a leap year 1 (leap year), 0 otherwise
+        /**
+         * ISO 8601 week-numbering year. This has the same value as Y,
+         * except that if the ISO week number (W) belongs to the previous
+         * or next year, that year is used instead.
+         */
+        case o = '${o}'; // Same as Y, except that it use week number to decide which year it falls onto
+        case Y = '${Y}'; // A full numeric representation of a year, at least 4 digits, with - for years BCE. 1991, 2012, 2014, ...
+        case y = '${y}'; // A two digit representation of a year 91, 12, 14, ...
+        // Time Format
+        case a = '${a}'; // Lowercase Ante meridiem and Post meridiem am or pm
+        case A = '${A}'; // Uppercase Ante meridiem and Post meridiem AM or PM
+        case B = '${B}'; // Swatch Internet time 000 through 999
+        case g = '${g}'; // 12-hour format of an hour without leading zeros 1 through 12
+        case G = '${G}'; // 24-hour format of an hour without leading zeros 0 through 23
+        case h = '${h}'; // 12-hour format of an hour with leading zeros 01 through 12
+        case H = '${H}'; // 24-hour format of an hour with leading zeros 01 through 23
+        case i = '${i}'; // Minutes with leading zeros 01 through 59
+        case s = '${s}'; // Seconds with leading zeros 00 through 59
+        case c = '${c}'; // 2004-02-12T15:19:21
+        case r = '${r}'; // Thu, 21 Dec 2000 16:01:07
+        case u = '${u}'; // Unix Timestamp (seconds since Jan 1 1970 00:00:00)
 
         // Runtime Macros
-        case PROJECT_PATH = '${PROJECT_PATH}';
         case CURRENT_WORKING_DIRECTORY = '${CWD}';
+        case PROCESS_ID = '${PID}';
+        case USER_ID = '${UID}';
+        case GLOBAL_ID = '${GID}';
+        case USER_HOME_PATH = '${HOME}';
 
         // Project Macros
+        case PROJECT_PATH = '${PROJECT_PATH}';
+        case DEFAULT_BUILD_CONFIGURATION = '${DEFAULT_BUILD_CONFIGURATION}';
+
+        // Assembly Macros
         case ASSEMBLY_NAME = '${ASSEMBLY.NAME}';
         case ASSEMBLY_PACKAGE = '${ASSEMBLY.PACKAGE}';
         case ASSEMBLY_VERSION = '${ASSEMBLY.VERSION}';
@@ -54,6 +91,11 @@
         case ASSEMBLY_COPYRIGHT = '${ASSEMBLY.COPYRIGHT}';
         case ASSEMBLY_TRADEMARK = '${ASSEMBLY.TRADEMARK}';
 
+        // Compiler Runtime Macros
+        case COMPILE_TIMESTAMP = '${COMPILE_TIMESTAMP}';
+        case NCC_BUILD_VERSION = '${NCC_BUILD_VERSION}';
+        case BUILD_OUTPUT_PATH = '${BUILD_OUTPUT_PATH}';
+
         /**
          * Handles built-in macro variables and returns their values.
          *
@@ -62,18 +104,46 @@
          */
         private static function builtinHandler(string $input): ?string
         {
+            $currentTime = time(); // Tiny optimization
+
             return match(self::tryFrom($input))
             {
-                self::DATE => date('Y-m-d'),
-                self::TIME => date('H:i:s'),
-                self::DATETIME => date('Y-m-d H:i:s'),
-                self::YEAR => date('Y'),
-                self::MONTH => date('m'),
-                self::DAY => date('d'),
-                self::HOUR => date('H'),
-                self::MINUTE => date('i'),
-                self::SECOND => date('s'),
+                // DateTime Macros
+                self::d => date('d', $currentTime),
+                self::D => date('D', $currentTime),
+                self::j => date('j', $currentTime),
+                self::l => date('l', $currentTime),
+                self::N => date('N', $currentTime),
+                self::S => date('S', $currentTime),
+                self::w => date('w', $currentTime),
+                self::z => date('z', $currentTime),
+                self::W => date('W', $currentTime),
+                self::F => date('F', $currentTime),
+                self::m => date('m', $currentTime),
+                self::M => date('M', $currentTime),
+                self::n => date('n', $currentTime),
+                self::t => date('t', $currentTime),
+                self::L => date('L', $currentTime),
+                self::o => date('o', $currentTime),
+                self::Y => date('Y', $currentTime),
+                self::y => date('y', $currentTime),
+                self::a => date('a', $currentTime),
+                self::A => date('A', $currentTime),
+                self::B => date('B', $currentTime),
+                self::g => date('g', $currentTime),
+                self::G => date('G', $currentTime),
+                self::h => date('h', $currentTime),
+                self::H => date('H', $currentTime),
+                self::i => date('i', $currentTime),
+                self::s => date('s', $currentTime),
+                self::c => date('c', $currentTime),
+                self::r => date('r', $currentTime),
+                self::u => $currentTime,
+
+                // Runtime Macros
                 self::CURRENT_WORKING_DIRECTORY => getcwd(),
+
+                // No matches
                 default => null
             };
         }
