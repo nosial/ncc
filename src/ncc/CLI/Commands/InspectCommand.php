c688e64bea18135006c39cc319ae05b3019d1940
netkas <netkas.nosial@gmail.com>
2025-12-07 16:47:25 -0500

    Add InspectCommand for package inspection and reference output

---
+++ src/ncc/CLI/Commands/InspectCommand.php
@@ -0,0 +1,116 @@
+<?php
+    /*
+     * Copyright (c) Nosial 2022-2025, all rights reserved.
+     *
+     *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+     *  associated documentation files (the "Software"), to deal in the Software without restriction, including without
+     *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+     *  Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+     *  conditions:
+     *
+     *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+     *  of the Software.
+     *
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+     *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+     *  PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+     *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+     *  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+     *  DEALINGS IN THE SOFTWARE.
+     *
+     */
+
+    namespace ncc\CLI\Commands;
+
+    use Exception;
+    use ncc\Abstracts\AbstractCommandHandler;
+    use ncc\Classes\Console;
+    use ncc\Classes\PackageReader;
+    use ncc\Objects\Package\ComponentReference;
+    use ncc\Objects\Package\ExecutionUnitReference;
+    use ncc\Objects\Package\ResourceReference;
+
+    class InspectCommand extends AbstractCommandHandler
+    {
+
+        /**
+         * @inheritDoc
+         */
+        public static function handle(array $argv): int
+        {
+            $packagePath = getcwd();
+            if(isset($argv['path']))
+            {
+                $packagePath = realpath($argv['path']);
+            }
+
+            if(!file_exists($packagePath) || !is_file($packagePath))
+            {
+                Console::error('The given path must be a ncc package file');
+                return 1;
+            }
+
+            try
+            {
+                $packageReader = new PackageReader($packagePath);
+            }
+            catch(Exception $e)
+            {
+                Console::error($e->getMessage());
+                return 1;
+            }
+
+            Console::out('Package Structure Version: ' . $packageReader->getPackageVersion());
+            Console::out('Build Number: ' . $packageReader->getHeader()->getBuildNumber());
+            if($packageReader->getHeader()->getEntryPoint())
+            {
+                Console::out('Executable: Yes');
+                Console::out('Entry Point: ' . $packageReader->getHeader()->getEntryPoint());
+            }
+            else
+            {
+                Console::out('Executable: No');
+            }
+
+            Console::out('Package: ' . $packageReader->getAssembly()->getPackage());
+            Console::out('Package Name: ' . $packageReader->getAssembly()->getName());
+            Console::out('Package Version: ' . $packageReader->getAssembly()->getVersion());
+
+            foreach($packageReader->getAllReferences() as $reference)
+            {
+                switch(get_class($reference))
+                {
+                    case ResourceReference::class:
+                        /** @var ResourceReference $reference */
+                        Console::out('Resource: ' . $reference->getName() . ' (Offset: ' . $reference->getOffset() . ', Size: ' . $reference->getSize() . ' bytes)');
+                        break;
+
+                    case ComponentReference::class:
+                        /** @var ComponentReference $reference */
+                        Console::out('Component: ' . $reference->getName() . ' (Offset: ' . $reference->getOffset() . ', Size: ' . $reference->getSize() . ' bytes)');
+                        break;
+
+                    case ExecutionUnitReference::class:
+                        /** @var ExecutionUnitReference $reference */
+                        Console::out('Execution Unit: ' . $reference->getName() . ' (Offset: ' . $reference->getOffset() . ', Size: ' . $reference->getSize() . ' bytes)');
+                        $executionUnit = $packageReader->readExecutionUnit($reference);
+                        Console::out('  - Name: ' . $executionUnit->getName());
+                        Console::out('  - Type: ' . $executionUnit->getType()->value);
+                        Console::out('  - Mode: ' . $executionUnit->getMode()->value);
+                        Console::out('  - Entry Point: ' . $executionUnit->getEntryPoint());
+                        Console::out('  - Working Directory: ' . $executionUnit->getWorkingDirectory());
+                        if(!is_null($executionUnit->getRequiredFiles()))
+                        {
+                            Console::out('  - Required Files: ' . implode($executionUnit->getRequiredFiles(), ', '));
+                        }
+                        break;
+
+                    default:
+                        Console::out('Unknown Reference Type: ' . get_class($reference));
+                        break;
+                }
+            }
+
+            return 0;
+        }
+    }
